2743ac0474f1c589f80b9cdd9ac6b5408514fc94
Allan Juan <allanjuansil@gmail.com>
2022-03-25 12:56:02 -0300

    Add hanoi towers problem definition

---
+++ exercises/towers-of-hanoi/towers-of-hanoi.ts
@@ -0,0 +1,10 @@
+/**
+ * Let `towers` be a variable of the type `HanoiTowers`.
+ *
+ * Each property represents a tower. The i-th number in each
+ * array represents the size of the i-th disk in that tower from top
+ * to bottom.
+ */
+type HanoiTowers = { left: number[]; middle: number[]; right: number[] };
+
+function solve(towers: HanoiTowers) {}
